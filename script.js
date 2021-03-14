50f0afe55158072abf98bd6bd4e239c2e7658bd2
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-03-14 14:58:59 +0700

    урок 3 усложненка

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 'use strict';
-
-function isNum(num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
+let lang = 'ru';
+if (lang === 'ru') {
+  console.log(['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']);
+} else if(lang === 'en'){
+  console.log([ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
 }
-  
-function guessTheNumber() {
-  
-  let luckyNumber = 56;
 
-  let enteredNumber = prompt('Угадай число от 1 до 100');
-  if (enteredNumber === null) {
-    alert('Игра окончена');
-    return enteredNumber;
-  }
-  
-  if (isNum(enteredNumber)) {
-    enteredNumber = +enteredNumber;
-     if (luckyNumber === enteredNumber) {
-     alert('Поздравляю, Вы угадали!!!');
-     return enteredNumber;
-    } else if (luckyNumber < enteredNumber) {
-     enteredNumber = alert ('Загаданное число меньше');
-    } else if (luckyNumber > enteredNumber) {
-     enteredNumber = alert ('Загаданное число больше');
-    }
- } else {
-   alert('Введи число!');
- }
- return guessTheNumber();
+
+
+switch(lang) {
+case 'ru':
+  console.log(['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']);
+  break;
+case 'en':
+  console.log([ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
 }
-guessTheNumber();
+
+let days = [];
+		days['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+		days['en'] = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+	console.log(days[lang]);
+
+
+ 
+
+
 
