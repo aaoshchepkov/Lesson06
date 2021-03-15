7422741ddc0d0de42d457aa1e3e56f114ca3363f
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-03-15 14:43:44 +0700

    урок 7 усложненка

--- script.js
+++ script.js
@@ -1,51 +1,13 @@
 'use strict';
 
-function isNum(num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
-} 
-let count = 0;
-let start = () => {
-let num = Math.floor(Math.random() * 101);
-function guessTheNumber() {
-  let enteredNumber = prompt('Угадай число от 1 до 100');
-  if (enteredNumber === null) {
-        alert('Игра окончена');
-        return ;
-     }
-  if (count === 10) {
-    let losses = confirm('Попытки закончились, хотите сыграть еще?');
-     if (losses === true) {
-      count = 0;
-      start (); 
-     } else {
-        alert('Игра окончена');
-        return ;
-     }
-  }
-  if (isNum(enteredNumber)) {
-    enteredNumber = +enteredNumber;
-     if (num === enteredNumber) {
-     let win = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-     if (win === true) {
-      count = 0;
-      start ();
-     } else {
-      alert('Игра окончена');
-      return ;
-     }
-    } else if (num < enteredNumber) {
-     enteredNumber = alert ('Загаданное число меньше, осталось ' + (10-count) + ' попыток');
-    } else if (num > enteredNumber) {
-     enteredNumber = alert ('Загаданное число больше, осталось ' + (10-count) + ' попыток');
-    }
- } else {
-   alert('Введи число!');
- }
- count+= 1;
- guessTheNumber();
-}
-guessTheNumber();
-};
-start();
-
-
+let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+   toDay = new Date().getDay() - 1;  
+week.forEach((item, i) => {
+  if (i === toDay) {
+     document.write(`<span><b>${item}</b></span><br>`);
+   } else if (item === 'сб' || item === 'вс') {
+     document.write(`<span><i>${item}</i></span><br>`);
+   } else {
+     document.write(`<span>${item}</span><br>`);
+   }
+}); 
