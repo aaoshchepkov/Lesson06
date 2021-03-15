a398d3c58bba87e86e868c2713b229dc83f7e286
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-03-15 16:21:13 +0700

    урок 9 усложненка

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
 'use strict';
+let post1 = document.createElement('p');
+let post2 = document.createElement('p');
+let start = ()=> {
+let toDay = new Date(),
+weekDay = toDay.getDay()-1,
+day = toDay.getDate(),
+month = toDay.getMonth()+1,
+year = toDay.getYear() + 1900,
+hour = toDay.getHours(),
+minute = toDay.getMinutes(),
+second = toDay.getSeconds(),
+nameHours = 'часов',
+week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+months = 
+['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
 
-function isNum(num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
-} 
-let count = 0;
-let start = () => {
-let num = Math.floor(Math.random() * 101);
-function guessTheNumber() {
-  let enteredNumber = prompt('Угадай число от 1 до 100');
-  if (enteredNumber === null) {
-        alert('Игра окончена');
-        return ;
-     }
-  if (count === 10) {
-    let losses = confirm('Попытки закончились, хотите сыграть еще?');
-     if (losses === true) {
-      count = 0;
-      start (); 
-     } else {
-        alert('Игра окончена');
-        return ;
-     }
+const timer1 = () => {
+  if(hour === 1 || hour === 21){
+    nameHours = 'час';
+  } else if(hour === 2 || hour === 3 || hour === 4 || hour === 22 || hour === 23 || hour === 24 ) 
+  {nameHours = 'часa';} 
+  week.forEach((item, i) =>{
+ if (i === weekDay) {
+  months.forEach((itemM, i) => {
+  if (month === i) {
+    let month = itemM;
+    post2.textContent = (`Сегодня ${item}, ${day} ${month} ${year} года, ${hour} ${nameHours} ${minute} мин ${second} сек`);
+    document.body.append(post2);
   }
-  if (isNum(enteredNumber)) {
-    enteredNumber = +enteredNumber;
-     if (num === enteredNumber) {
-     let win = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-     if (win === true) {
-      count = 0;
-      start ();
-     } else {
-      alert('Игра окончена');
-      return ;
-     }
-    } else if (num < enteredNumber) {
-     enteredNumber = alert ('Загаданное число меньше, осталось ' + (10-count) + ' попыток');
-    } else if (num > enteredNumber) {
-     enteredNumber = alert ('Загаданное число больше, осталось ' + (10-count) + ' попыток');
-    }
- } else {
-   alert('Введи число!');
+  }); 
  }
- count+= 1;
- guessTheNumber();
+});
+};
+timer1();
+
+const timer2 = () => {
+if (day <=9){
+  day = '0' + day;
 }
-guessTheNumber();
+if (month <=9){
+  month = '0' + month;
+}
+if (hour <=9){
+  hour = '0' + hour;
+}
+if (minute <=9){
+  minute = '0' + minute;
+}
+if (second <=9){
+ second = '0' + second;
+} 
+post1.textContent = (`${day}.${month}.${year} - ${hour}:${minute}:${second}`);
+document.body.append(post1);
+};
+timer2();
+
 };
 start();
 
+setInterval((start),1000);
 
